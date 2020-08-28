5b7151e30ad1c245a6de954a79cda60887c32058
mschirbel <mschirbel.g@gmail.com>
2020-08-28 17:19:21 -0300

    only 2 subnets should be ok

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -5,8 +5,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = data.aws_availability_zones.available.zone_ids
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   enable_nat_gateway = true
 
   tags = merge(
