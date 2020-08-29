b44dd21e88859e089f5f1cb4342ecd8a9caae5c1
mschirbel <mschirbel.g@gmail.com>
2020-08-29 20:05:16 -0300

    vpc outputs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,3 +1,15 @@
 output alb-dns {
     value = module.alb.this_lb_dns_name
 }
+
+output public_subnets_id {
+    value = module.vpc.private_subnets
+}
+
+output private_subnets_id {
+    value = module.vpc.public_subnets
+}
+
+output main_vpc_id {
+    value = module.vpc.vpc_id
+}
