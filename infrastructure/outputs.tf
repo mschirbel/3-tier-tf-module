41aa95e7540b7153d080e2ffe3c13122b6e4fcda
mschirbel <mschirbel.g@gmail.com>
2020-09-01 12:12:37 -0300

    adding rds outputs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -13,3 +13,11 @@ output private_subnets_id {
 output main_vpc_id {
     value = module.vpc.vpc_id
 }
+
+output rds_id {
+    value = module.db.this_db_instance_id
+}
+
+output rds_connection_string_parameter {
+    value = aws_ssm_parameter.rds_connection_string.name
+}
